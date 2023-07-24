fecfac1291728a5f1a31db9b1fccbb0789243ae3
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-07-24 07:47:16 +0530

    Update project1_1.py

--- project1_1.py
+++ project1_1.py
@@ -1,8 +1,11 @@
+#importing required modules.
 import mains
 from matplotlib import pyplot as plt
 teams=mains.teams_list
+#Calculating total runs of batting team.
 for delivery in mains.deliveries_reader:
     teams[delivery['batting_team']]+= int(delivery['total_runs'])
+#Plotting total runs scored by teams.
 plt.bar(list(teams.keys()),list(teams.values()))
 plt.xlabel=('Teams')
 plt.ylabel=('Total Runs')
