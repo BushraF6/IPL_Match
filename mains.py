dee30ebb2425eecc51cae0b3d178a2ff9e0f93d7
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-07-26 23:56:02 +0530

    Added Comments.

--- mains.py
+++ mains.py
@@ -1,22 +1,2 @@
-import csv
 
-deliveriesfile=open('deliveries.csv', 'r', encoding= 'utf8')
-deliveries_reader= csv.DictReader(deliveriesfile)
-teams_list={'Sunrisers Hyderabad': 0, 
-            'Royal Challengers Bangalore': 0, 
-            'Mumbai Indians': 0, 
-            'Rising Pune Supergiant': 0, 
-            'Gujarat Lions': 0, 
-            'Kolkata Knight Riders': 0, 
-            'Kings XI Punjab': 0, 
-            'Delhi Daredevils': 0, 
-            'Chennai Super Kings': 0, 
-            'Rajasthan Royals': 0, 
-            'Deccan Chargers': 0, 
-            'Kochi Tuskers Kerala': 0, 
-            'Pune Warriors': 0, 
-            'Rising Pune Supergiants': 0}
-
-matchesfile= open('matches.csv', 'r', encoding='utf8')
-matches_reader=csv.DictReader(matchesfile)
 
