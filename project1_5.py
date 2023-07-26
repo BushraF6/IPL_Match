2beea7e7a07c89fc57b0dfd6ede51a0a3480bc19
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-07-27 00:03:44 +0530

    Added Comments.

--- project1_5.py
+++ project1_5.py
@@ -1,15 +1,18 @@
-import csv
+""" Import the required modules to solve the problem. """
 from matplotlib import pyplot as plt
+import mains
 
-year_match_count={str(i):0 for i in range(2008,2018)}
-with open('matches.csv','r') as matchesfile:
-    mathches_reader= csv.DictReader(matchesfile) 
-    for match in mathches_reader:
-        year_match_count[match['season']]+=1
-matches_count=list(year_match_count.values())
-years=list(year_match_count.keys())
-
-plt.bar(years,matches_count)
+matches = mains.matches_reader
+# Creating dictionary of years with default value 0.
+year_match_count = {str(i): 0 for i in range(2008, 2018)}
+# Calculate total matches played per year.
+for match in matches:
+    year_match_count[match['season']] += 1
+# Creating 2 lists for storing the years and matches_count.
+matches_count = list(year_match_count.values())
+years = list(year_match_count.keys())
+# Plotting the calculated data.
+plt.bar(years, matches_count)
 plt.xlabel('Year')
 plt.ylabel('No: of Matches played')
 plt.show()
