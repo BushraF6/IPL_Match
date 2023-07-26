d580dbcfc24f35b838571022008370610026ba91
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-07-27 00:00:08 +0530

    Added Comments.

--- project1_3.py
+++ project1_3.py
@@ -1,14 +1,20 @@
+""" Importing required modules to solve the problem. """
 import csv
 from matplotlib import pyplot as plt
 
-umpires_country={}
-with open('umpires.csv','r') as umpiresfile:
-    umpire_reader=csv.DictReader(umpiresfile)
+# Create dictionary to store no: of umpires from each country.
+umpires_country = {}
+# Extracting data from umpires.csv file.
+with open('umpires.csv', 'r', encoding='utf-8') as umpiresfile:
+    umpire_reader = csv.DictReader(umpiresfile)
+    # Calculate no: of umpires from each country except India.
     for umpire in umpire_reader:
         if umpire[' country'] not in umpires_country:
-            umpires_country[umpire[' country']]=0
-        umpires_country[umpire[' country']]+=1
-    del(umpires_country[' India'])
-plt.bar(list(umpires_country.keys()),list(umpires_country.values()))
-plt.xlabel,plt.ylabel=('Countries'),('No: of umpires')
+            umpires_country[umpire[' country']] = 0
+        umpires_country[umpire[' country']] += 1
+    del umpires_country[' India']
+# Plot the calculated data.
+plt.bar(list(umpires_country.keys()), list(umpires_country.values()))
+plt.xlabel('Countries')
+plt.ylabel('No_of_Umpires')
 plt.show()
